f386a47f5f47d9b7197222771112c6556f2e2750
Robson <devwebrobson@gmail.com>
2024-04-03 16:29:47 -0300

    first commit

---
+++ evento.js
@@ -0,0 +1,12 @@
+/*
+    Eventos 
+
+*/
+
+const input = document.querySelector("main-input")
+const select = document.querySelector("#main-select")
+const button = document.querySelector("main-button")
+
+select.addEventListener ("change", function () {
+    console.log("troquei de valor ")
+})
